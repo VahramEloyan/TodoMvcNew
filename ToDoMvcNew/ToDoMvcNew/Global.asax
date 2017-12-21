ab4beb2aa6d0946ee64fc984f2846401593c7c39
vahrameloyan <vahrameloyan@gmail.com>
2017-12-21 17:46:58 +0400

    Added ToDoMvc Part 1

---
+++ ToDoMvcNew/ToDoMvcNew/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToDoMvcNew.MvcApplication" Language="C#" %>
---
+++ ToDoMvcNew/ToDoMvcNew/Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoMvcNew.Models
+{
+    public class ToDo
+    {
+        public int Id { set; get; }
+        public string Description { set; get; }
+        public bool IsDone { set; get; }
+        public ApplicationUser User { set; get; }
+    }
+}
